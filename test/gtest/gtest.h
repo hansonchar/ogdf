e39956f6f9a9f27a17e06e831aa571f610be65bd
Hanson Char <hanson.char@gmail.com>
2024-06-21 14:18:52 -0700

    Fix stuff so ./makeMAkefile.sh would work

--- test/gtest/gtest.h
+++ test/gtest/gtest.h
@@ -1554,11 +1554,11 @@ inline bool operator!=(const GTEST_10_TUPLE_(T)& t,
 // <tr1/functional>.  Hence the following #define is a hack to prevent
 // <tr1/functional> from being included.
 #   define _TR1_FUNCTIONAL 1
-#   include <tr1/tuple>
+#   include <tuple>
 #   undef _TR1_FUNCTIONAL  // Allows the user to #include
                         // <tr1/functional> if he chooses to.
 #  else
-#   include <tr1/tuple>  // NOLINT
+#   include <tuple>  // NOLINT
 #  endif  // !GTEST_HAS_RTTI && GTEST_GCC_VER_ < 40302
 
 # else
